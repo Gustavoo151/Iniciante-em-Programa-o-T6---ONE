28d55619312121f1f9e26e52dc516444ca04c74b
gustavoo151 <josegustavooliveira121@gmail.com>
2024-01-17 19:36:16 -0300

    Adicionando aula de introdução e desafios de 1 à 10

---
+++ Introducao/app.js
@@ -0,0 +1,26 @@
+/*
+alert('Boas Vindas ao jogo do Mundo secreto');
+let numeroSecreto = 29;
+let chute = 0;
+let numeroTentativa = 0;
+
+
+while(true){
+    chute = prompt('Escolha um número de secreto entre 1 a 30:');
+    numeroTentativa++;
+
+    if (numeroSecreto != chute){
+        if (chute < numeroSecreto){
+            console.log('Errou! Digite Número Maior!');
+        }
+        else{
+            console.log('Errou! Digite Número Menor!');
+        }
+    }
+    else{
+        console.log('Você acertou o número secreto' + numeroSecreto + ' Com ' + numeroTentativa + ' Tentativas.');
+        break;
+    }
+}
+*/
+
---
+++ Introducao/desafios.js
@@ -0,0 +1,33 @@
+// Desafio 1:
+alert('Boas VIndas ao nosso site!');
+
+// Desafio 2:
+let lua = 'Lua';
+
+// Desafio 3:
+let idade = 25;
+
+// Desafio 4:
+let numeroVendas = 50;
+
+// Dasafio 5:
+let saldoDisponivel = 1000;
+
+// Desafio 6:
+alert('Erro! Preencha todos os campos.');
+
+// Desfio 7:
+let mesagemDeErro = 'Erro! Preencha todos os campos.';
+alert(mesagemDeErro);
+
+// Desafio 8:
+let nome;
+nome = prompt('Digite seu nome: ');
+
+// Desafio 9:
+idade = prompt('Digite sua idade: ');
+
+// Desafio 10:
+if (idade >= 18){
+    alert('Pode tirar a habilitação!')
+}
