cb3b1f16cbcbef22a482ba386edf2782e5813016
gustavoo151 <josegustavooliveira121@gmail.com>
2024-01-19 13:27:38 -0300

    Aula 01

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 4;
+console.log(numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 10');
+
+// Comentário
+
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+}
+else{
+    alert('Você errou!');
+}
