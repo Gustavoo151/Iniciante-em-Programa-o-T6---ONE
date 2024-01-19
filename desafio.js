4e5f6cb726ede09496ff0cb2053936b95f8c2dd3
gustavoo151 <josegustavooliveira121@gmail.com>
2024-01-19 14:23:47 -0300

    Desafio adicionados

---
+++ desafio.js
@@ -0,0 +1,42 @@
+//Desafios
+//1- Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+/*let diaAtual = prompt("Digite o dia atual: ");
+
+if (diaAtual == 'Sabado' || diaAtual == 'Domingo'){
+    alert("Bom fim de semana!");
+}
+else{
+    alert("Boa semana!");
+}*/
+
+//2- Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+/*let numeroDigitado = prompt("Digite um número: ");
+
+if (numeroDigitado >= 0){
+    alert("Positivo!");
+}
+else{
+    alert("Negativo!");
+}
+*/
+
+//3- Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+/*let pontuação = prompt("Digite sua pontuação: ");
+
+if (pontuação < 100){
+    alert("Tente novamente para ganhar.");
+}
+else{
+    alert("Parabéns, você venceu!");
+}*/
+
+//4- Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+/*let saldo = prompt("Digite seu saldo:");
+
+alert(`Seu saldo é ${saldo}`);*/
+
+//5- Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+/*let nomeUsuario = prompt("Digite seu nome: ");
+
+alert(`Bem Vindo, ${nomeUsuario}`);
+*/
