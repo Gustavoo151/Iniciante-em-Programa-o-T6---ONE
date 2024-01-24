8d32bb27e0d543e16370ba335236fbcdf17d42a3
gustavoo151 <josegustavooliveira121@gmail.com>
2024-01-24 03:32:02 -0300

    Criação funções com paramentros para reutlizar código

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-let titulo = document.querySelector('h1'); // Aqui estamos selecionando a tag que queremos alterar no código.   
+//let titulo = document.querySelector('h1'); // Aqui estamos selecionando a tag que queremos alterar no código.   
+//titulo.innerHTML = 'Jogo do número secreto';  // Essa frase vai ser jogada dentro do h1
 
-titulo.innerHTML = 'Jogo do número secreto';  // Essa frase vai ser jogada dentro do h1
+//let paragrafo = document.querySelector('p'); // Aqui estamos selecionando a tag que queremos alterar no código.  
+//paragrafo.innerHTML = 'Escreva um número entro 1 e 10:'; // Essa frase vai ser jogada dentro do h1
 
-let paragrafo = document.querySelector('p'); // Aqui estamos selecionando a tag que queremos alterar no código.  
+function exibirTextoNaTela(tag, texto){   // Função para exibir o texto na tela
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-paragrafo.innerHTML = 'Escreva um número entro 1 e 10:'; // Essa frase vai ser jogada dentro do h1
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+
+exibirTextoNaTela('p', 'Escreva um número entro 1 e 10:');
 
 function verificarChute() {
     console.log('O botão foi clicado!');
-}
+}   
