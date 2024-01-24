b72b97143804d91c1552fb9cf12925a47e457ac0
gustavoo151 <josegustavooliveira121@gmail.com>
2024-01-24 12:17:47 -0300

    Função para mostrar texto na tela

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-//let titulo = document.querySelector('h1'); // Aqui estamos selecionando a tag que queremos alterar no código.   
-//titulo.innerHTML = 'Jogo do número secreto';  // Essa frase vai ser jogada dentro do h1
-
-//let paragrafo = document.querySelector('p'); // Aqui estamos selecionando a tag que queremos alterar no código.  
-//paragrafo.innerHTML = 'Escreva um número entro 1 e 10:'; // Essa frase vai ser jogada dentro do h1
-
 function exibirTextoNaTela(tag, texto){   // Função para exibir o texto na tela
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -11,7 +5,7 @@ function exibirTextoNaTela(tag, texto){   // Função para exibir o texto na tel
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 
-exibirTextoNaTela('p', 'Escreva um número entro 1 e 10:');
+exibirTextoNaTela('p', 'Escreva um número entre 1 e 10:');
 
 function verificarChute() {
     console.log('O botão foi clicado!');
